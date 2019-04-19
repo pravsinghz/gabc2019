17f64b5b4b20a7c8776b578cb1e0c2c6c7d33b7d
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2019-04-19 06:54:03 +0530

    Adding v1 build site

---
+++ docs/precache-manifest.7a51f812f7a0390f5ee30fe2591bb43d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "93c70dba4b0606b186de67b630ed184d",
+    "url": "/static/media/raghuram.93c70dba.png"
+  },
+  {
+    "revision": "172d573e3ccf3b45194e",
+    "url": "/static/css/main.e8c191fc.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7b0776f451bbf0a49ec3",
+    "url": "/static/js/2.d5189d33.chunk.js"
+  },
+  {
+    "revision": "af4055ebd442d76999e38ebb7cc2a990",
+    "url": "/static/media/home-hero.af4055eb.jpg"
+  },
+  {
+    "revision": "172d573e3ccf3b45194e",
+    "url": "/static/js/main.a9b7d3d1.chunk.js"
+  },
+  {
+    "revision": "e510ab33f699e7fc6c5f853eec99589c",
+    "url": "/static/media/shashi.e510ab33.jpeg"
+  },
+  {
+    "revision": "e161f74296c37429db4a5010527e485c",
+    "url": "/static/media/subramanyam.e161f742.png"
+  },
+  {
+    "revision": "2db4163bbcf8de2b604ede0baf157066",
+    "url": "/static/media/thrilok.2db4163b.jpg"
+  },
+  {
+    "revision": "2c1afe1810942ab4b4d5f79f9029ee85",
+    "url": "/static/media/viral.2c1afe18.jpg"
+  },
+  {
+    "revision": "a37c9669130a7320ab92a07d4a4949a2",
+    "url": "/index.html"
+  }
+];
